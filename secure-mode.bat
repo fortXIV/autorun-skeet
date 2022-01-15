d63c283ad48c3c209272031a1a088f6285c79024
fortXIV <97703963+fortXIV@users.noreply.github.com>
2022-01-15 16:37:15 +0100

    Update secure-mode.bat

--- secure-mode.bat
+++ secure-mode.bat
@@ -1,5 +1,5 @@
 for /r "." %%a in (*.exe) do start "" "%%~fa" --load=1
 SLEEP
-cd C:\Program Files (x86)\Steam
-START steam.exe -applaunch 730
+::cd C:\Program Files (x86)\Steam
+::START steam.exe -applaunch 730
 cls
