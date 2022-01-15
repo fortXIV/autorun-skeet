f02e5c8227aeba42958826bd51d9d2986ffd80fe
fortXIV <97703963+fortXIV@users.noreply.github.com>
2022-01-15 16:37:00 +0100

    Update insecure-mode.bat

--- insecure-mode.bat
+++ insecure-mode.bat
@@ -1,5 +1,5 @@
 for /r "." %%a in (*.exe) do start "" "%%~fa" --load=1
 SLEEP
-cd C:\Program Files (x86)\Steam
-START steam.exe -applaunch 730 -insecure
+::cd C:\Program Files (x86)\Steam
+::START steam.exe -applaunch 730 -insecure
 cls
