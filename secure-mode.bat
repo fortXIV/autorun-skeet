e92dd76f946fb8ce5b13cd60567e0537367de4be
fortXIV <97703963+fortXIV@users.noreply.github.com>
2022-01-30 14:58:06 +0100

    update
    no longer needs to manually input install path, it grabs it automatically

--- secure-mode.bat
+++ secure-mode.bat
@@ -1,5 +1,3 @@
 for /r "." %%a in (*.exe) do start "" "%%~fa" --load=1
-SLEEP
-::cd C:\Program Files (x86)\Steam
-::START steam.exe -applaunch 730
-cls
+for /f "tokens=1,2,* " %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam" /v "InstallPath"') do set "path=%%k"
+start "CSGO" "%path%/Steam.exe" -applaunch 730
